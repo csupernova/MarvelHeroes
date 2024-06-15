0609a6c67ee063bd66a801e632ff2bbe2c3ad329
dmitry <mr.kargapolov@gmail.com>
2024-06-15 17:02:35 +0600

    [~] move api keys to local.properties

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -6,6 +8,8 @@ plugins {
     id("com.google.dagger.hilt.android")
     id("com.google.gms.google-services")
 }
+val apiPublicKey: String = gradleLocalProperties(rootDir).getProperty("apiPublicKey")
+val apiPrivateKey: String = gradleLocalProperties(rootDir).getProperty("apiPrivateKey")
 
 android {
     namespace = "com.example.marvelheroes"
@@ -23,10 +27,13 @@ android {
             useSupportLibrary = true
         }
     }
-
     buildTypes {
+        debug {
+            buildConfigField("String", "apiPublicKey",  apiPublicKey)
+            buildConfigField("String", "apiPrivateKey",  apiPrivateKey)
+        }
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -42,6 +49,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -76,7 +84,7 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.6.0")
 
     //retrofit
-    implementation("com.squareup.retrofit2:converter-moshi:2.4.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
